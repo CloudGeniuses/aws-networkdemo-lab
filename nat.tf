af91e7157afebed1e8b8e33ff255e1e918623ae6
CloudGeniuses <132877816+CloudGeniuses@users.noreply.github.com>
2025-10-29 04:09:16 -0600

    Create nat.tf

---
+++ nat.tf
@@ -0,0 +1,39 @@
+############################################
+# NAT Gateway (public-a) + Private RT routes
+############################################
+
+# Elastic IP for NAT GW
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+  tags = {
+    Name = "${var.project_prefix}-nat-eip"
+  }
+}
+
+# NAT Gateway in the public-a subnet
+resource "aws_nat_gateway" "nat" {
+  subnet_id     = aws_subnet.public_a.id        # cg-adv-subnet-public-a
+  allocation_id = aws_eip.nat_eip.id
+  tags = {
+    Name = "${var.project_prefix}-natgw-a"
+  }
+
+  # Ensure the IGW exists before NAT GW (good practice)
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# Collect your private route tables
+locals {
+  private_route_tables = {
+    private_a = aws_route_table.private_a.id
+    private_b = aws_route_table.private_b.id
+  }
+}
+
+# Default route in each private RT via the NAT GW
+resource "aws_route" "private_default_via_nat" {
+  for_each                  = local.private_route_tables
+  route_table_id            = each.value
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id            = aws_nat_gateway.nat.id
+}
